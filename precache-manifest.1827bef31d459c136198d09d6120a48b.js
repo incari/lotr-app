563959fb2ed9d148993d30f29e9c3bc02afec186
Martin Racana <ing.racana@gmail.com>
2020-05-17 20:05:41 +0200

    Updates

---
+++ precache-manifest.1827bef31d459c136198d09d6120a48b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff29a762352954bdce20068d884bd28",
+    "url": "/lotr-app/index.html"
+  },
+  {
+    "revision": "cb5e31a78d84ca263cec",
+    "url": "/lotr-app/static/css/main.b9a733be.chunk.css"
+  },
+  {
+    "revision": "01474a91d59a5af4786e",
+    "url": "/lotr-app/static/js/2.c9ac3fbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lotr-app/static/js/2.c9ac3fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5e31a78d84ca263cec",
+    "url": "/lotr-app/static/js/main.cdc3d978.chunk.js"
+  },
+  {
+    "revision": "f1fcad6f3abd2091470d",
+    "url": "/lotr-app/static/js/runtime-main.de52cd82.js"
+  },
+  {
+    "revision": "5b6ecd6efaa07f8d9dced38874240636",
+    "url": "/lotr-app/static/media/backgroud.5b6ecd6e.jpg"
+  },
+  {
+    "revision": "8b615882c42b7cb3213605a4008cbbae",
+    "url": "/lotr-app/static/media/logo-lotr.8b615882.png"
+  }
+]);
